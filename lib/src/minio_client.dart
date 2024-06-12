e4491bb00e68a5e4b07367524cb2fe2dcb798c11
Antonio Sánchez <444620+plutec@users.noreply.github.com>
2024-06-12 16:52:09 +0900

    Update minio_client.dart

--- lib/src/minio_client.dart
+++ lib/src/minio_client.dart
@@ -160,7 +160,7 @@ class MinioClient {
     print(b2);
     print("this.b2:");
     print(this.b2);*/
-    if ((b2 == null) || (b2 == false)) { 
+    if ((minio.b2 == null) || (minio.b2 == false)) { 
       print("b2 es null");
       authorization = signV4(minio, request, date, region); 
     } else {
